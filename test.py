21d2576ad1ff80abe3df2825e4ae08afa3b80073
Edwin <edwin@datux.nl>
2012-04-27 21:12:36 +0200

    moving stuff around

--- test.py
+++ test.py
@@ -1,57 +1,37 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-from wsgiref.simple_server import make_server
-from beaker.middleware import SessionMiddleware
-import beaker
-
-import models.ticket.invoice
-#import models.core.users
+import beaker.middleware
+import bottle
 
 
+#rpc calls to models:
+@bottle.route('/rpc')
+def rpc():
+	s = bottle.request.environ.get('beaker.session')
+	return "rpc"
 
+#serve static content from the static dir
+#(in production the webserver should do this)
+@bottle.route('/static/<filename:path>')
+def send_static(filename):
+    return bottle.static_file(filename, root='static')
 
-
-def application(environ, start_response):
-	# Sorting and stringifying the environment key, value pairs
-	response_body = ['%s: %s' % (key, value)
-	                 for key, value in sorted(environ.items())]
-	response_body = '\n'.join(response_body)
-
-	session=environ['beaker.session']
-	if 'counter' in session:
-		session['counter']=session['counter']+1
-	else:
-		session['counter']=0
-	
-	session.save()
-
-#	response_body="geert {}\n".format(session['counter'])
-
-	status = '200 OK'
-	response_headers = [('Content-Type', 'text/plain'),
-			('Content-Length', str(len(response_body)))]
-	start_response(status, response_headers)
-
-	return [response_body]
-
-   
 session_opts = {
     'session.type': 'file',
     'session.cookie_expires': True,
     'session.data_dir': '/tmp/testsessions'
 }
-wsgi_app = SessionMiddleware(application, session_opts)
-
-# Instantiate the WSGI server.
-# It will receive the request, pass it to the application
-# and send the application's response to the client
-httpd = make_server(
-   'localhost', # The host name.
-   8051, # A port number where to wait for the request.
-   wsgi_app # Our application object name, in this case a function.
-   )
 
 
+app = beaker.middleware.SessionMiddleware(
+		bottle.default_app()
+	,session_opts)
 
-httpd.handle_request()
-#httpd.serve_forever()
+
+#debug mode
+bottle.debug(True)
+bottle.run(reloader=True, app=app, host='localhost', port=8080)
+
+#production:
+#bottle.run(app=app, host='localhost', port=8080)
+
