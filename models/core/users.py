958d7ba104e694d0fbdb18378ad85252a9198c74
Edwin <edwin@datux.nl>
2012-05-16 01:18:22 +0200

    zzz

--- models/core/users.py
+++ models/core/users.py
@@ -1,24 +1,7 @@
-import core.model
+from models.common import *
 
 
-class acl(object):
-    def __init__(self, groups=["admin"]):
-        self.groups = groups
-
-    def __call__(self, f):
-        def wrapped_f(wrapped_instance, *args):
-            print "in wrapperrr"
-            print self.groups
-            #...controleer of de gebruk
-            f(wrapped_instance, *args)
-
-        #we want to be able to verify if the outer wrapper is an acl_wrapper            
-        wrapped_f.has_acl_decorator=1
-        return wrapped_f
-
-        
-
-class users():
+class Users(Base):
     def __init__(self):
         print "init users"
         
