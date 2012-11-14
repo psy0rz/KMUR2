0508c03ef3a05acb3bb77a76f467771dbe5590fd
Edwin <edwin@datux.nl>
2012-11-14 21:40:12 +0100

    fields.js now uses plain js prototypes (classes are not really needed and give too much overhead and internal state in this case)

--- static/fields.js
+++ static/fields.js
@@ -1,85 +1,117 @@
-//construct a new fieldobject from metadata
-//key should correspond with the dotted key to reach our metadata. its used to resolve html elements to operate on.
-function FieldBase(meta, key)
-{
-    //store a reference to the metadata
-    this.meta=meta;
-    this.key=key;
-}
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-/***
-    context     :jquery context to operate in
-    data        :data to put in
+/*** All fieldtypes are stored in a dictorany so we can easily look them up.
 */
+Field={};
 
-//create input elements from metadata
-FieldBase.prototype.create=function(context)
+
+
+/*** Fields base prototype
+    Most publicly callable functions have these parameters:
+        key         :key of the specified meta data. Usually also stored in context.attr("field-key")
+        meta        :meta-data 
+        context     :jquery dom element to operate on or in
+        data        :data to put in
+*/
+Field.Base={};
+
+/*** generic function thats used when something is not implemented. 
+
+    tries to make the error visible and logs it to the console.
+    */
+Field.Base.not_implemented=function(key, meta, context, data)
 {
-
+    context.empty();
+    context.append($("<span class='ui-state-error'>Not implemented: "+key));
+    console.error("Not-implemented field-function called: ", key, meta, context, data);
 }
 
-//create html code from data
-FieldBase.prototype.create_html=function(context, data)
+/*** put specified meta-datafield literally into context
+ 
+    this is usually used to show the description of a field to the user, or things like min and max values.
+    context should have class field-meta-put
+    context should have an attribute called 'field-meta-key' that specfies the metadata key
+*/
+Field.Base.meta_put=function(key, meta, context)
 {
-
+    var meta_key=context.attr("field-meta-key")];
+    if (meta_key in meta)
+        context.text(meta[meta_key]);
+    else
+        context.text("");
 }
 
-//put metadata fields in existing element
-FieldBase.prototype.put_meta=function(context)
+/*** appends specified input element into the context
+
+    mostly used internally by input_create
+    this will modify the input-element as well!
+    automaticly sets field-key attribute.
+    automaticly adds field-input-put class.
+    when meta.readonly is not true, automaticly adds field-input-get class, otherwise disables input element.
+    automacly sets title to meta.desc.
+    */ 
+Field.Base.input_append=function(key, meta, context, input)
 {
-
-}
-
-//store data in existing input elements
-FieldBase.prototype.put=function(context,data)
-{
-}
-
-//get data from existing input elements and return it
-FieldBase.prototype.get=function(context)
-{
-}
-
-
-Field['Dict']=function(meta, key)
-{
-    FieldBase.call(this, meta, key);
-
-    //we're a dict: create all the subfields from the specified sub-metadata.
-    for (meta_key in meta.meta)
+    context.addClass("field-input-put");
+    context.attr("field-key", key);
+    context.attr("title", meta.desc);
+    
+    if (!meta.readonly)
     {
-        var sub_key;
-        if (key)
-            sub_key=key+"."+meta_key;
-        else
-            sub_key=meta_key;
-
-        /*** 
-        yeah.. try to wrap your head around this one..
-        this.fields will be a object that contains all the sub-field object-instances.
-
-        example:
-        meta={
-            'type':'Dict',
-            'desc':'the root dict object',
-            'meta':{
-                        'somefieldname':{
-                            'type':'String',
-                            'desc':'Some string'    
-                        }
-            }
-        }
-
-        now this constructor will do something like this:
-        this.fields['somefieldname']=new Field[String]( { 'type':'String', 'desc':'Some string' }  , 'somefieldname')
-        
-        
-
-        */
-        this.fields={};
-        this.fields[meta_key]=new Field[meta.meta[sub_key].type](meta.meta[sub_key], sub_key);
-
+        context.addClass("field-input-get");
     }
+    else
+    {
+        context.attr('disabled',true);
+    }
+    context.empty();
+    context.append(element);
+};
+
+
+/*** create input element from metadata and store it in the context
+
+    context should have class field-input-create
+    newly created input elements automaticly get apporpiate classes and attributes. (see input_append)
+    */
+Field.Base.input_create=Field.Base.not_implemented;
+
+
+/*** puts data into existing input element
+
+    context should be a input element that was created by input_create
+    context should have class field-input-put
+    */
+Field.Base.input_put=Field.Base.not_implemented;
+
+
+/*** gets data from existing input element and returns it.
+    context should be an input-field was created by input_create 
+    context should have class field-input-get
+*/
+Field.Base.input_get=Field.Base.not_implemented;
+
+
+/*** convert data into html and store it in the context
+    context should have class field-html-create    
+*/
+Field.Base.html_create=Field.Base.not_implemented;
+{
+
 }
-Field['Dict'].prototype=Object.create(FieldBase.prototype);
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/*** Dictonary type. This is usually the base fieldtype we start with.
+
+*/
+Field.Dict=Object.create(Field.Base);
+
+
+Field.Dict.input_create=function(key, meta, context)
+{
+
+}
 
