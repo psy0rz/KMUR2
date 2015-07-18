795ee179532e1e7f3ec25141d41a60ab4b1823d3
Super-User <root@volg.email>
2015-07-18 20:01:01 +0000

    smartos stuff

---
+++ settings-example.py
@@ -0,0 +1,9 @@
+#Rename this to settings.py and provide an api_key
+
+#api_key="...insert some random key here..."
+
+#Enable these settings when running in a SmartOS zone with mod_wsgi
+#import os
+#os.environ["MAGICK_HOME"]="/opt/local"
+#os.environ["PATH"]+=":/opt/local/bin"
+
