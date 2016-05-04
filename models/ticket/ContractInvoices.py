f7d4e56ce0b2a619980cf06b4a0333ae2ecb1745
Edwin Eefting <edwin@datux.nl>
2016-05-04 17:08:02 +0200

    fix

--- models/ticket/ContractInvoices.py
+++ models/ticket/ContractInvoices.py
@@ -630,7 +630,7 @@ class ContractInvoices(models.core.Protected.Protected):
                 if ticket_object['minutes_factor']!=1:
                     invoice_desc=invoice_desc+" (calculated at {}% rate)".format(ticket_object['minutes_factor']*100)
 
-                if 'start_time' in ticket_object:
+                if 'start_time' in ticket_object and ticket_object["start_time"]:
                     work_time=ticket_object['start_time']
                 else:
                     work_time=ticket_object['create_time']
